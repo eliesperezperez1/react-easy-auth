b0db1a56fc5711f340e8b88ec126e4c08bc6a1c7
HectorMartinezM <hmartinezm@valencia.es>
2024-06-25 10:37:25 +0200

    Export button filtered rows

--- src/components/export-button/export-button.tsx
+++ src/components/export-button/export-button.tsx
@@ -4,34 +4,30 @@ import * as XLSX from "xlsx";
 import { saveAs } from "file-saver";
 import { MutableRefObject, useState } from "react";
 import { GridApiCommunity } from "@mui/x-data-grid/models/api/gridApiCommunity";
+import { gridFilteredSortedRowIdsSelector, gridVisibleColumnFieldsSelector } from '@mui/x-data-grid';
 
 export interface ExportButtonProps {
   visibleData: MutableRefObject<GridApiCommunity>;
 }
 
-function ExportButton({ visibleData }: ExportButtonProps) {
+ function ExportButton({ visibleData }: ExportButtonProps) {
+
   const [openMenuExportar, setOpenMenuExportar] = useState(false);
 
   const getVisibleData = () => {
-    const rowModels = Array.from(
-      visibleData.current.getVisibleRowModels().values()
-    );
-    const visibleColumns = visibleData.current
-      .getVisibleColumns()
-      .map((column) => column.field);
+    const filteredSortedRowIds = gridFilteredSortedRowIdsSelector(visibleData);
+    const visibleColumnsField = gridVisibleColumnFieldsSelector(visibleData);
 
-    return rowModels.map((obj) => {
-      const newObj = { ...obj };
-      delete newObj._id;
-
-      Object.keys(newObj).forEach((key) => {
-        if (!visibleColumns.includes(key)) {
-          delete newObj[key];
-        }
+    // Format the data. Here we only keep the value
+    const data = filteredSortedRowIds.map((id) => {
+      const row: any = {};
+      visibleColumnsField.forEach((field) => {
+        row[field] = visibleData.current.getCellParams(id, field).value;
       });
-
-      return newObj;
+      return row;
     });
+
+    return data;
   };
 
   const handleExport = (type: "xlsx" | "json") => {
@@ -50,7 +46,7 @@ function ExportButton({ visibleData }: ExportButtonProps) {
 
     setOpenMenuExportar(false);
   };
-
+ 
   return (
     <ClickAwayListener onClickAway={() => setOpenMenuExportar(false)}>
       <Tooltip
@@ -109,6 +105,6 @@ function ExportButton({ visibleData }: ExportButtonProps) {
       </Tooltip>
     </ClickAwayListener>
   );
-}
+} 
 
 export default ExportButton;
